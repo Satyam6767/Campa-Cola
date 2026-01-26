f0754f486a8d31e6237715c866fd9bd3355cb714
Satyam6767 <satyamkumar6767@gmail.com>
2026-01-26 16:46:54 +0530

    initil-commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  baseURL: "https://campa-cola.onrender.com/",
+  baseURL: "https://campa-cola.onrender.com/api",
 });
 
 export default API;
