a6c783a7afad38ba090dfe9164b273b82f55fc29
Satyam6767 <satyamkumar6767@gmail.com>
2026-01-27 01:42:55 +0530

    initial-commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import Register from "./pages/Register";
 import Cart from "./pages/Cart";
 import Profile from "./pages/Profile";
 import ProductDetails from "./pages/ProductDetails";
-import Checkout from "./pages/CheckOut";
+import Checkout from "./pages/Checkout";
 import OrderSuccess from "./pages/OrderSuccess";
 import Orders from "./pages/Orders";
 
