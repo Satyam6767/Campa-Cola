2b18624299d726df674fe5708053b7b632396e49
Satyam6767 <satyamkumar6767@gmail.com>
2026-03-06 12:37:03 +0530

    LC

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -68,7 +68,7 @@ const Footer = () => {
         {/* COPYRIGHT */}
         <Box className="footer-bottom">
           <Typography className="footer-copy">
-            © 2026 Janki Enterprises | Campa Cola Distributor
+            © 2026 Janki Enterprises | Campa Cola Distributor  |  Design and Developed by Millionis Tech
           </Typography>
         </Box>
       </Container>
