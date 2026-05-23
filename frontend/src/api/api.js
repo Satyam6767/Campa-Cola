5fd70daf6f7df84560bad3619ae9db638e640cfe
satyam6767 <satyamkumar6767@gmail.com>
2026-05-24 02:24:35 +0530

    LC

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
-  // baseURL: "https://campa-cola-1.onrender.com/api",
-  baseURL: "https://janki-random.onrender.com"
+  baseURL: "https://campa-cola-1.onrender.com/api",
 });
 
 export default API;
