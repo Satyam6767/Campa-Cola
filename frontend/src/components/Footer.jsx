f2c659dcfc1cc6c844d33e7620cc9ff382d933b8
satyam6767 <satyamkumar6767@gmail.com>
2026-06-28 10:41:34 +0530

    LC

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -62,7 +62,7 @@ const Footer = () => {
             <Box className="footer-map">
               <iframe
   title="Janki Enterprises Location"
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3571.6334006465154!2d85.6976011!3d26.4675435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ec517093337179%3A0xa3f23c0bc6354aaf!2sJanki%20Enterprises!5e0!3m2!1sen!2sin!4v1775821137938!5m2!1sen!2sin"
+  src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d617.3071862942337!2d85.69986313482154!3d26.473547908716775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjbCsDI4JzI0LjIiTiA4NcKwNDInMDEuOSJF!5e0!3m2!1sen!2sin!4v1782622496076!5m2!1sen!2sin"
   width="100%"
   height="180"
   style={{ border: 0 }}
